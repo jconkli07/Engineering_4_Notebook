b7219d91c7c271eec941af2730aefecc1643d4f3
jconkli07 <jconkli07@charlottesvilleschools.org>
2023-05-30 10:36:27 -0400

    update CA1

--- raspberry-pi/CrashAvoidance1.py
+++ raspberry-pi/CrashAvoidance1.py
@@ -1,11 +1,13 @@
 import time
 import board
 import adafruit_mpu6050
+import busio
 
-sda_pin=board.GP12
-scl_pin=board.GP13
-i2c = board.I2C(scl_pin, sda_pin)
+sda_pin=board.GP10
+scl_pin=board.GP11
+i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
 while True:
     print(mpu.acceleration)
+    time.sleep(0.5)
